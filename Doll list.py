11dd874afd3ae4eee9f9f996da7b6820ca91586b
Jinelyn Mangubat <mangubatjinelyn@gmail.com>
2024-09-20 06:17:43 -0700

    list,py

---
+++ Doll list.py
@@ -0,0 +1,3 @@
+("15th index is is" , list[14])
+list[5]= "barbie" "tom" "jerry" "princess" "bear"
+print ("last index is",list[-2])
---
+++ siblings list.py
@@ -0,0 +1,2 @@
+list [11]= "1" "2" "3" "4"
+print ("last index is"[-2])
